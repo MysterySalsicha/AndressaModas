e6ab1ed22ef87462cfc55c5f55d55e58e4fe3087
MysterySalsicha <igordasilvasantos38@gmail.com>
2025-06-15 20:08:40 -0300

    correções

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,14 +3,16 @@ import { Pool } from 'pg';
 import { PostgresJsAdapter } from "@lucia-auth/adapter-postgresql";
 import { cookies } from "next/headers";
 import { cache } from "react";
+import type { Session, User } from "lucia";
 
+// Adapta a conexão do 'node-postgres' para o Lucia
 const pool = new Pool({
   connectionString: process.env.POSTGRES_URL,
 });
 
 const adapter = new PostgresJsAdapter(pool, {
   user: "usuarios",
-  session: "sessoes_usuario" // Nome da tabela de sessões
+  session: "sessoes_usuario"
 });
 
 export const lucia = new Lucia(adapter, {
@@ -29,14 +31,11 @@ export const lucia = new Lucia(adapter, {
   }
 });
 
-// Função para validar a requisição e obter o usuário
-export const validateRequest = cache(async () => {
+// Valida a sessão do usuário em cada requisição
+export const validateRequest = cache(async (): Promise<{ user: User; session: Session } | { user: null; session: null }> => {
 	const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
 	if (!sessionId) {
-		return {
-			user: null,
-			session: null
-		};
+		return { user: null, session: null };
 	}
 
 	const result = await lucia.validateSession(sessionId);
@@ -53,7 +52,7 @@ export const validateRequest = cache(async () => {
 	return result;
 });
 
-
+// Tipagem para o Lucia entender os campos do nosso usuário
 declare module "lucia" {
   interface Register {
     Lucia: typeof lucia;
